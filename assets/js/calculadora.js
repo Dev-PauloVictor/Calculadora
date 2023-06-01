21e9bafefe3c642e40c877cacd95af23a72b0d0c
Victor9i <paulovgomes23@gmail.com>
2023-06-01 14:21:56 -0300

    feat: added some js code changes..

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,9 +1,15 @@
-
 function insert(num) {
+    //document.getElementById('resultado').innerHTML = num;
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
- 
+
+function calcularPorcentagem() {
+    var numero = document.getElementById('resultado').innerHTML;
+    var resultado = parseFloat(numero) / 100;
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
 function clean() {
     document.getElementById('resultado').innerHTML = "";
 }
@@ -14,3 +20,4 @@ function calcular() {
         document.getElementById('resultado').innerHTML = eval(resultado)
     }
 }
+//Um valor String que contém um código JavaScript válido.
